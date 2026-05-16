b89eeb72811b344d33c4af82ee54dd550f2d89ad
Mazigh <guettafmazigh@gmail.com>
2026-05-16 18:21:15 +0200

    feat: add class-validator and class-transformer for DTO validation

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,14 @@
-export class CreateArticleDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+export class CreateArticleDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  title!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
+  @MaxLength(5000)
+  content!: string;
+}
--- src/articles/dto/update-article.dto.ts
+++ src/articles/dto/update-article.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateArticleDto } from './create-article.dto';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class UpdateArticleDto extends PartialType(CreateArticleDto) {}
+export class UpdateArticleDto extends PartialType(CreateArticleDto) {
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(3)
+    @MaxLength(255)
+    title!: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(10)
+    @MaxLength(5000)
+    content!: string;
+
+}

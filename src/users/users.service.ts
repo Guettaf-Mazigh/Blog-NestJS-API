194b01ce83b66001d0d61753d881110082822feb
Mazigh <guettafmazigh@gmail.com>
2026-05-17 13:47:05 +0200

    feat: implement user update and delete functionality with validation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(private readonly usersRepository: Repository<User>) {}
+
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number) {
+    const user = this.usersRepository.findOneBy({ id });
+    if(!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.usersRepository.preload({
+      id,
+      ...updateUserDto,
+    });
+    if(!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    return this.usersRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const result = await this.usersRepository.delete(id); 
+    if(result.affected === 0) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    return { deleted: true };
+  }
+}
